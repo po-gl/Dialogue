9678af42ce6e91fe0f2ee20f3c5f0acc962b140d
po-gl <porterglines@gmail.com>
2022-12-30 14:10:25 -0700

    Initial Commit

---
+++ Dialogue/DialogueApp.swift
@@ -0,0 +1,17 @@
+//
+//  DialogueApp.swift
+//  Dialogue
+//
+//  Created by Porter Glines on 12/30/22.
+//
+
+import SwiftUI
+
+@main
+struct DialogueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DialogueUITests/DialogueUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  DialogueUITestsLaunchTests.swift
+//  DialogueUITests
+//
+//  Created by Porter Glines on 12/30/22.
+//
+
+import XCTest
+
+final class DialogueUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}

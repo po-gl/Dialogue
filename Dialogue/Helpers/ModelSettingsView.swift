5a658021e832ee8d24a18381679156e2d856550e
po-gl <porterglines@gmail.com>
2023-03-04 07:28:13 -0700

    Add heading to settings for iOS

--- Dialogue/Helpers/ModelSettingsView.swift
+++ Dialogue/Helpers/ModelSettingsView.swift
@@ -14,60 +14,24 @@ struct ModelSettingsView: View {
     @Binding var isPresented: Bool
     
     var body: some View {
-        VStack (spacing: 10) {
 #if os(iOS)
-            RoundedRectangle(cornerRadius: 30)
-                .frame(width: 50, height: 5)
-                .opacity(0.2)
-                .padding(5)
-#endif
-            GroupBox {
-                VStack (alignment: .leading, spacing: 0) {
-                    HStack (spacing: 0) {
-                        Text("Max tokens:")
-                            .font(.system(size: 14, weight: .medium))
-                            .padding(.trailing, 20)
-                        Text("\(Int(maxTokens))")
-                            .font(.system(size: 14, weight: .medium))
-                        Text("  ≈  \(Int(maxTokens*0.75)) words")
-                            .font(.system(size: 16))
-                            .opacity(0.6)
-                    }
-                    .padding(.bottom, 10)
-                    .padding(.horizontal, 0)
-                    Slider(value: $maxTokens, in: 150...2000, step: 50) {
-                        Text("Max tokens")
-                    } minimumValueLabel: {
-                        Text("150")
-                    } maximumValueLabel: {
-                        Text("2000")
-                    }
-                    .tint(Color("ServerAccent"))
-                }
+        ZStack {
+            SettingsHeader()
+            VStack (spacing: 15) {
+                TokenSetting()
+                MemorySetting()
+                Spacer()
             }
+            .padding(.top, 75)
+            .padding(25)
+        }
+        .presentationDetents([.medium])
+        
+#elseif os(OSX)
+        VStack (spacing: 15) {
+            TokenSetting()
+            MemorySetting()
             
-            GroupBox {
-                VStack (alignment: .leading, spacing: 0) {
-                    HStack (spacing: 0) {
-                        Text("Memory:")
-                            .font(.system(size: 14, weight: .medium))
-                            .padding(.trailing, 20)
-                        Text("\(Int(messageMemory)) messages")
-                            .font(.system(size: 14, weight: .medium))
-                    }
-                    .padding(.bottom, 10)
-                    .padding(.horizontal, 0)
-                    Slider(value: $messageMemory, in: 2...12, step: 1) {
-                        Text("Max tokens")
-                    } minimumValueLabel: {
-                        Text("2")
-                    } maximumValueLabel: {
-                        Text("12")
-                    }
-                    .tint(Color("UserAccent"))
-                }
-            }
-#if os(OSX)
             HStack {
                 Spacer()
                 Button(action: { isPresented = false} ) {
@@ -76,12 +40,81 @@ struct ModelSettingsView: View {
                 .padding(.top)
             }
             .frame(width: 300)
-#elseif os(iOS)
-            Spacer()
-#endif
         }
         .padding(.top, 10)
         .padding(25)
-        .presentationDetents([.height(250)])
+        .presentationDetents([.medium])
+#endif
+    }
+    
+    
+    @ViewBuilder private func SettingsHeader() -> some View {
+        VStack {
+            ZStack {
+                HStack {
+                    Button("Done") { isPresented = false }
+                        .foregroundColor(Color("ServerAccent"))
+                        .padding()
+                    Spacer()
+                }
+                Text("Model Settings")
+                    .font(.title3)
+            }
+            .frame(height: 65)
+            .background(.thinMaterial)
+            Spacer()
+        }
+    }
+    
+    @ViewBuilder private func TokenSetting() -> some View {
+        GroupBox {
+            VStack (alignment: .leading, spacing: 0) {
+                HStack (spacing: 0) {
+                    Text("Max tokens:")
+                        .font(.system(size: 14, weight: .medium))
+                        .padding(.trailing, 20)
+                    Text("\(Int(maxTokens))")
+                        .font(.system(size: 14, weight: .medium))
+                    Text("  ≈  \(Int(maxTokens*0.75)) words")
+                        .font(.system(size: 16))
+                        .opacity(0.6)
+                }
+                .padding(.bottom, 10)
+                .padding(.horizontal, 0)
+                Slider(value: $maxTokens, in: 150...2000, step: 50) {
+                    Text("Max tokens")
+                } minimumValueLabel: {
+                    Text("150")
+                } maximumValueLabel: {
+                    Text("2000")
+                }
+                .tint(Color("ServerAccent"))
+            }
+        }
+    }
+    
+    
+    @ViewBuilder private func MemorySetting() -> some View {
+        GroupBox {
+            VStack (alignment: .leading, spacing: 0) {
+                HStack (spacing: 0) {
+                    Text("Memory:")
+                        .font(.system(size: 14, weight: .medium))
+                        .padding(.trailing, 20)
+                    Text("\(Int(messageMemory)) messages")
+                        .font(.system(size: 14, weight: .medium))
+                }
+                .padding(.bottom, 10)
+                .padding(.horizontal, 0)
+                Slider(value: $messageMemory, in: 2...12, step: 1) {
+                    Text("Max tokens")
+                } minimumValueLabel: {
+                    Text("2")
+                } maximumValueLabel: {
+                    Text("12")
+                }
+                .tint(Color("UserAccent"))
+            }
+        }
     }
 }
